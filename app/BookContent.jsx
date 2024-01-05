49d322065b3757e47c9f22bf2eed022b57c71386
lamehillbilly <99287057+lamehillbilly@users.noreply.github.com>
2024-01-04 19:33:18 -0500

    fixed mobile

--- app/BookContent.jsx
+++ app/BookContent.jsx
@@ -3,7 +3,8 @@ import Link from 'next/link';
 const myFont = localFont({ src: './dpcomic.ttf' })
 
 export const BookContent = () => (
-    <div className="p-8 justify-center flex w-[800px] h-[370px] bg-[url('/main-content.png')] bg-no-repeat text-2xl">
+  <>
+    <div className="p-8 justify-center flex w-[800px] h-[370px] bg-[url('/main-content.png')] bg-no-repeat max-md:hidden text-2xl">
       <div className={myFont.className}>
         <div className="text-3xl font-bold text-center">KIMBO is the #1 dog coin on the Avalanche Blockchain.</div>
         <div className=" pt-5 flex w-full justify-center items-center text-center">
@@ -15,9 +16,27 @@ export const BookContent = () => (
         </div>
         <div className="flex font-bold text-center flex-col m-5 "> 🪙69,420,000,000 Tokens🪙</div>
         <div className="flex flex-col text-center items-center m-5"><Link href="https://snowtrace.io/tx/0xc3482f010b53a8a5681f5054d590200b896a711ae28c786aefe74c1250814f0f">🔥Liquidity Burned🔥</Link></div>
-        <div className="flex font-bold text-cente m-5 flex-col"><div className="flex flex-col text-center">❌Contract Renoucned❌</div> </div>
+        <div className="flex font-bold text-cente m-5 flex-col"><div className="flex flex-col text-center">❌Contract Renounced❌</div> </div>
         <div className="flex font-bold text-cente m-5 flex-col text-center"><div className="flex flex-col text-center">Contract:</div> <div className="flex flex-col text-[22px]"><Link href="https://snowtrace.io/token/0x184ff13B3EBCB25Be44e860163A5D8391Dd568c1"> 0x184ff13B3EBCB25Be44e860163A5D8391Dd568c1</Link></div></div>
       </div>
     </div>
+
+    <div className="justify-center flex  md:hidden text-2xl overflow-hidden bg-[#1f294a] m-4 rounded-lg bg-opacity-65 py-4">
+      <div className={myFont.className}>
+        <div className="text-3xl font-bold text-center">KIMBO is the #1 dog coin on the Avalanche Blockchain.</div>
+        <div className=" pt-5 flex w-full justify-center items-center text-center">
+          
+          
+          
+          
+
+        </div>
+        <div className="flex font-bold text-center flex-col m-5 "> 🪙69,420,000,000 Tokens🪙</div>
+        <div className="flex flex-col text-center items-center m-5"><Link href="https://snowtrace.io/tx/0xc3482f010b53a8a5681f5054d590200b896a711ae28c786aefe74c1250814f0f">🔥Liquidity Burned🔥</Link></div>
+        <div className="flex font-bold text-cente m-5 flex-col"><div className="flex flex-col text-center">❌Contract Renounced❌</div> </div>
+        <div className="flex font-bold text-cente m-5 flex-col text-center"><div className="flex flex-col text-center">Contract:</div> <div className="flex flex-col text-[22px] underline"><Link href="https://snowtrace.io/token/0x184ff13B3EBCB25Be44e860163A5D8391Dd568c1"> CLICK HERE</Link></div></div>
+      </div>
+    </div>
+    </>
   );
   
